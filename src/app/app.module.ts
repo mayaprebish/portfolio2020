5e11b5711aed37e6ff5537f3993db4ef6844adf2
Maya Prebish <mprebish@blueport.com>
2020-05-15 13:06:00 -0400

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,17 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ParallaxDirective } from './parallax.directive';
+import { AboutComponent } from './about/about.component';
+import { WorkComponent } from './work/work.component';
+import { ContactComponent } from './contact/contact.component';
+import { ResumeComponent } from './resume/resume.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, ParallaxDirective, AboutComponent, WorkComponent, ContactComponent, ResumeComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
