f6c8602c5f2999806f412424e5e0b1cfa67499d3
mayaprebish <prebish.m@husky.neu.edu>
2020-05-20 22:51:08 -0400

    layout

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { WorkComponent } from './work/work.component';
 import { ContactComponent } from './contact/contact.component';
 import { ResumeComponent } from './resume/resume.component';
 import { HeaderComponent } from './header/header.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, FormsModule, BrowserAnimationsModule ],
   declarations: [ AppComponent, ParallaxDirective, AboutComponent, WorkComponent, ContactComponent, ResumeComponent, HeaderComponent ],
   bootstrap:    [ AppComponent ]
 })
